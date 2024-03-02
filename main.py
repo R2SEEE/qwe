78ec17662bd8492d153a2fbf85ee6786196367d2
DaniPK23 <burnosenkodanil@gmail.com>
2024-03-02 17:55:10 +0700

    start new project

--- main.py
+++ main.py
@@ -1,35 +1,16 @@
-import sys
+"""
+точка
+"""
+
+from logical import check
 
 
-def fun_val(n, k):
+def main():
     """
-    функция определяет число Вудала и
-    определяеться ли оно кратным 3-ем
+    функция входа
     """
-    if (n + 2) > k:
-        x = n * k ** n - 1
-        if x % 3 == 0:
-            print("полученное число является кратным трём.")
-        else:
-            print("полученное число не является кратным трём.")
-
-    else:
-        print("данное число не являеться числом Вудала")
-
-
-def check():
-    while True:
-        n_num, k_num = input('Введи число n: '), input('Введи число k: ')
-        try:
-            fun_val(float(n_num), float(k_num))
-            person = input("Хотите выйти yes or not: ")
-            if person == "yes":
-                sys.exit(1)
-            else:
-                print("Повторите ввод")
-        except:
-            print("Ошибка входных данных, повторите ввод")
+    check()
 
 
 if "__main__" == __name__:
-    check()
+    main()
